69066c4798d1d89b89134605bf4969e9a258a54e
Rummut <kzantip@gmail.com>
2023-08-20 03:40:48 +0300

    Update ImageGalleryItem.jsx

--- src/components/image-gallery-item/ImageGalleryItem.jsx
+++ src/components/image-gallery-item/ImageGalleryItem.jsx
@@ -1,13 +1,13 @@
 export const ImageGalleryItem = ({ imageState}) => {
   return (
     <>
-      {imageState.hits.map(hit => (
+      {/* {imageState.hits.map(hit => (
         <>
           <li key={hit.id}>
             <img src={hit.userImageURL} alt={hit.tags} />
           </li>
         </>
-      ))}
+      ))} */}
     </>
   );
 };

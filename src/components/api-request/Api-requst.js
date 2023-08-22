d34a7d5f288a77330307235be375944c55db870d
Rummut <kzantip@gmail.com>
2023-08-22 21:25:58 +0300

    Update Api-requst.js

--- src/components/api-request/Api-requst.js
+++ src/components/api-request/Api-requst.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export const FetchImages = async ({ page, values }) => {
-  console.log(values);
   const response = await axios.get(
     `https://pixabay.com/api/?q=${values}&page=${page}&key=38182607-aab9c53a0cd2b78abfbdb0e24&image_type=photo&orientation=horizontal&per_page=12`
   );
